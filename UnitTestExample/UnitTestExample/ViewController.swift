47a36fe3dd9e04f4e5ec06f1ebf76ff6b723911b
Daesiker <eotlr680@naver.com>
2022-11-18 17:35:26 +0900

    add UI

--- UnitTestExample/UnitTestExample/ViewController.swift
+++ UnitTestExample/UnitTestExample/ViewController.swift
@@ -6,14 +6,147 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    private let label:UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Log In"
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let username:UITextField = {
+        let field = UITextField()
+        field.placeholder = "Username"
+        field.translatesAutoresizingMaskIntoConstraints = false
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.backgroundColor = .secondarySystemBackground
+        field.layer.cornerRadius = 8
+        field.layer.borderWidth = 0.5
+        field.layer.borderColor = UIColor.label.cgColor
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 50))
+        field.leftViewMode = .always
+        return field
+    }()
+    
+    private let password:UITextField = {
+        let field = UITextField()
+        field.placeholder = "Password"
+        field.isSecureTextEntry = true
+        field.translatesAutoresizingMaskIntoConstraints = false
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.backgroundColor = .secondarySystemBackground
+        field.layer.cornerRadius = 8
+        field.layer.borderWidth = 0.5
+        field.layer.borderColor = UIColor.label.cgColor
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 50))
+        field.leftViewMode = .always
+        return field
+    }()
+    
+    private let button:UIButton = {
+       let button = UIButton()
+        button.setTitle("Continue", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemBlue
+        button.titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
+        button.layer.cornerRadius = 8
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(label)
+        view.addSubview(username)
+        view.addSubview(password)
+        view.addSubview(button)
+        addConstraints()
+        button.addTarget(self, action: #selector(login), for: .touchUpInside)
     }
 
+    
+    private func addConstraints() {
+        
+        label.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(100)
+            $0.centerX.equalToSuperview()
+        }
+        
+        username.snp.makeConstraints {
+            $0.top.equalTo(label.snp.bottom).offset(50)
+            $0.leading.equalToSuperview().offset(30)
+            $0.trailing.equalToSuperview().offset(-30)
+            $0.height.equalTo(56)
+        }
+        
+        password.snp.makeConstraints {
+            $0.top.equalTo(username.snp.bottom).offset(10)
+            $0.leading.equalToSuperview().offset(30)
+            $0.trailing.equalToSuperview().offset(-30)
+            $0.height.equalTo(56)
+        }
+        
+        button.snp.makeConstraints {
+            $0.top.equalTo(password.snp.bottom).offset(30)
+            $0.leading.equalToSuperview().offset(50)
+            $0.trailing.equalToSuperview().offset(-50)
+            $0.height.equalTo(56)
+        }
+        
+    }
+    
+    @objc func login() {
+        
+        guard let username = username.text,
+              let password = password.text,
+              username == "afraz",
+              password == "password" else {
+            let alert = UIAlertController(title: "Invalid Login", message: "Your login credentials were incorrect. Try again.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+            present(alert, animated: true)
+            return
+        }
+        
+        let vc = AccountVC()
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true)
+        
+    }
+    
 
 }
 
+
+final class AccountVC: UIViewController {
+    
+    private let label:UILabel = {
+       let label = UILabel()
+        label.textAlignment = .center
+        label.text = "Welcome, Afraz!"
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 32, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.widthAnchor.constraint(equalToConstant: 300),
+            label.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+}
