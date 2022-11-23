7488b3d170a2145f8b97f4d2b0e05370ffb91b0a
Daesiker <eotlr680@naver.com>
2022-11-23 13:23:20 +0900

    add Network Protocol

---
+++ UnitTestExample/SupportingFiles/NetworkMonitor.swift
@@ -0,0 +1,38 @@
+//
+//  NetworkMonitor.swift
+//  UnitTestExample
+//
+//  Created by Jun on 2022/11/23.
+//
+
+import Foundation
+import Network
+
+class NetworkMonitor {
+    
+    static let shared = NetworkMonitor()
+    
+    private let monitor = NWPathMonitor()
+    private var status = NWPath.Status.requiresConnection
+    
+    private init() {
+        startMonitoring()
+    }
+    
+    func startMonitoring() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            self?.status = path.status
+        }
+        
+        let queue = DispatchQueue(label: "NetworkMonitor")
+        monitor.start(queue: queue)
+        
+    }
+    
+    func stopMonitoring() {
+        monitor.cancel()
+    }
+    
+    
+    
+}
